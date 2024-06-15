ba8966ab18b494e67b963b680d2d29c048bf8c12
Mwakanemela <102220067+Mwakanemela@users.noreply.github.com>
2024-06-15 07:54:09 +0000

    finishing with use effect hook and local storage for keeping data even on reload

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,6 +1,6 @@
 import TodoInput from "./components/TodoInput.jsx";
 import TodoList from "./components/TodoList.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 function App() {
   // let todos = [];
 
@@ -8,13 +8,18 @@ function App() {
 
   const [todoValue, setTodoValue] = useState("");
 
+  function persistenceData(newTodo) {
+    localStorage.setItem("todos", JSON.stringify({ todos: newTodo }));
+  }
   function handleAddedTodos(newTodo) {
     const newTodoList = [...todos, newTodo];
+    persistenceData(newTodoList);
     setTodos(newTodoList);
   }
 
   function handleDeleteTodo(index) {
     const newTodoList = todos.filter((todo, i) => i !== index);
+      persistenceData(newTodoList);
     setTodos(newTodoList);
   }
 
@@ -23,6 +28,22 @@ function App() {
     setTodoValue(todoTobeEdited);
     handleDeleteTodo(index);
   }
+
+  useEffect(() => {
+    // if local storage does not exists
+    if (!localStorage) {
+      return;
+    }
+
+    let localTodos = localStorage.getItem("todos");
+
+    if (!localTodos) {
+      return;
+    }
+
+    localTodos = JSON.parse(localTodos).todos;
+    setTodos(localTodos);
+  }, []);
   // const editTodoList = (index) => {
   //   const todoTobeEdited = todos[index];
   //   setTodoValue(todoTobeEdited);
